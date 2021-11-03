ec271b3130bd8b353fa452be85b0da1426577f98
Austin Betts <65870500+Flashlock@users.noreply.github.com>
2021-11-02 18:06:23 -0700

    Confirm that Slick's TiledMap is broken. I don't think it fully supports isometric maps.

--- src/dracula_punch/States/TestState.java
+++ src/dracula_punch/States/TestState.java
@@ -1,5 +1,6 @@
 package dracula_punch.States;
 
+import dracula_punch.TiledMap.DPTiledMap;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
@@ -21,7 +22,8 @@ public class TestState extends BasicGameState {
     @Override
     public void enter(GameContainer container, StateBasedGame game) throws SlickException {
         super.enter(container, game);
-        tiledMap = new TiledMap(DraculaPunchGame.MAP);
+        tiledMap = new DPTiledMap(DraculaPunchGame.MAP);
+        int floorID = tiledMap.getLayerIndex("Floor");
     }
 
     @Override
@@ -34,8 +36,14 @@ public class TestState extends BasicGameState {
         graphics.drawString("Test State", 10, 25);
         DraculaPunchGame dpg = (DraculaPunchGame) stateBasedGame;
 
-        // this point is the top of the diamond
-        tiledMap.render(dpg.screenWidth / 2, 0);
+        // number of tiles to render
+        int width = 50;
+        int height = 50;
+        int pxHeight = height * tiledMap.getTileHeight();
+
+        // point it starts rendering from is the top of the diamond
+        tiledMap.render(dpg.screenWidth /2,dpg.screenHeight / 2 - pxHeight / 2-200,
+                0,0,width,height, true);
     }
 
     @Override
