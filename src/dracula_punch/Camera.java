54c244212bc03f80167e7ef9c89c81338ad1b734
EngrRitta <73570900+EngrRitta@users.noreply.github.com>
2021-11-08 00:08:58 -0800

    Implemented scroll+zoom, smooth isometric tile based motion and scrolling world

---
+++ src/dracula_punch/Camera.java
@@ -0,0 +1,65 @@
+package dracula_punch;
+
+import org.newdawn.slick.tiled.TiledMap;
+
+public class Camera {
+  public float zoomFactor = 1.0f;
+  public Coordinate isometric = new Coordinate();
+  public boolean moveUp, moveDown, moveLeft, moveRight, zoomIn, zoomOut;
+  private float previousZoom = 1.0f;
+  private float currentZoom = 1.0f;
+  private Coordinate currentTile = new Coordinate();
+  private Coordinate previousTile = new Coordinate();
+  private float totalMoveTime = 100;
+  private float movingTime = 99; // one less than total to trigger calculation once on startup
+  private float percentMoveDone;
+  private TiledMap map;
+
+  public Camera(TiledMap map) {
+    this.map = map;
+  }
+
+  public void update(int delta) {
+    if (movingTime == totalMoveTime) { initializeCameraMovement(); }
+    else { calculateCameraPosition(delta); }
+  }
+
+  private void initializeCameraMovement() {
+    int dx = 0;
+    int dy = 0;
+    float dz = 0f;
+    if (moveLeft && currentTile.x > 0) { dx = -1; }
+    if (moveRight && currentTile.x < map.getWidth() - 1) { dx = 1; }
+    if (moveUp && currentTile.y > 0) { dy = -1; }
+    if (moveDown && currentTile.y < map.getHeight() - 1) { dy = 1; }
+    if (zoomOut) { dz = -0.2f; }
+    if (zoomIn) { dz = 0.2f; }
+    if (dx!=0 || dy!=0 || dz!=0) { moveCamera(dx, dy, dz); }
+  }
+
+  private void moveCamera(int dx, int dy, float dz) {
+    movingTime = 0;
+    previousTile.setEqual(currentTile);
+    previousZoom = currentZoom;
+    currentTile.add(dx, dy);
+    currentZoom += dz;
+    // System.out.println(currentTile.x + " " + currentTile.y + " " + currentZoom);
+  }
+
+  private void calculateCameraPosition(int delta) {
+    movingTime += delta;
+    if(movingTime > totalMoveTime) { movingTime = totalMoveTime; }
+    percentMoveDone = (totalMoveTime - movingTime) / totalMoveTime;
+    float partialX = 0, partialY = 0, partialZ = 0;
+    if (previousTile.x > currentTile.x) { partialX = percentMoveDone; }
+    else if (previousTile.x < currentTile.x) { partialX = -percentMoveDone; }
+    if (previousTile.y > currentTile.y) { partialY = percentMoveDone; }
+    else if (previousTile.y < currentTile.y) { partialY = -percentMoveDone; }
+    if (previousZoom > currentZoom) { partialZ = 0.2f * percentMoveDone; }
+    else if (previousZoom < currentZoom) { partialZ = 0.2f * -percentMoveDone; }
+    Coordinate currentTilePlusPartial = new Coordinate(currentTile);
+    currentTilePlusPartial.add(partialX, partialY);
+    isometric = currentTilePlusPartial.getIsometricFromTile(map);
+    zoomFactor = currentZoom + partialZ;
+  }
+}
--- src/dracula_punch/States/TestState.java
+++ src/dracula_punch/States/TestState.java
@@ -1,5 +1,6 @@
 package dracula_punch.States;
 
+import dracula_punch.Camera;
 import dracula_punch.Coordinate;
 import dracula_punch.TiledMap.DPTiledMap;
 import org.newdawn.slick.GameContainer;
@@ -15,11 +16,7 @@ import org.newdawn.slick.tiled.TiledMap;
 public class TestState extends BasicGameState {
 
   private TiledMap map;
-  private int tilesInWindowX = 20;
-  private int tilesInWindowY = 20;
-  private Coordinate cameraTile = new Coordinate();
-  private float movingTime;
-  private float cycleTime = 100;
+  private Camera camera;
   private DraculaPunchGame dpg;
 
   @Override
@@ -31,7 +28,9 @@ public class TestState extends BasicGameState {
   public void enter(GameContainer container, StateBasedGame game) throws SlickException {
     super.enter(container, game);
     DraculaPunchGame dpg = (DraculaPunchGame)game;
+
     map = new DPTiledMap(DraculaPunchGame.MAP);
+    camera = new Camera(map);
     int floorID = map.getLayerIndex("Floor");
   }
 
@@ -42,52 +41,37 @@ public class TestState extends BasicGameState {
   @Override
   public void render(GameContainer gameContainer, StateBasedGame stateBasedGame, Graphics graphics) throws SlickException {
     graphics.drawString("Test State", 10, 25);
-
+    graphics.scale(camera.zoomFactor, camera.zoomFactor);
+    int tilesInWindowX = 100;
+    int tilesInWindowY = 100;
+    float screenOffsetX = DraculaPunchGame.screenWidth / camera.zoomFactor / 2;
+    float screenOffsetY = DraculaPunchGame.screenHeight / camera.zoomFactor / 2;
+    int x = (int)(camera.isometric.x+screenOffsetX);
+    int y = (int)(camera.isometric.y+screenOffsetY);
     // render layers individually to avoid Slick2d bug
-    Coordinate iso = toIso(cameraTile);
-    map.render(iso.x, iso.y, cameraTile.x, cameraTile.y, tilesInWindowX, tilesInWindowY, 0, true);
-    map.render(iso.x, iso.y, cameraTile.x, cameraTile.y, tilesInWindowX, tilesInWindowY, 1, true);
-    map.render(iso.x, iso.y, cameraTile.x, cameraTile.y, tilesInWindowX, tilesInWindowY, 2, true);
-    map.render(iso.x, iso.y, cameraTile.x, cameraTile.y, tilesInWindowX, tilesInWindowY, 3, true);
-    graphics.fillOval(dpg.screenWidth / 2 - 5, dpg.screenHeight / 2 - 5, 10, 10);
+    map.render(x, y, 0, 0, tilesInWindowX, tilesInWindowY, 0, true);
+    map.render(x, y, 0, 0, tilesInWindowX, tilesInWindowY, 1, true);
+    map.render(x, y, 0, 0, tilesInWindowX, tilesInWindowY, 2, true);
+    map.render(x, y, 0, 0, tilesInWindowX, tilesInWindowY, 3, true);
+    map.render(x, y, 0, 0, tilesInWindowX, tilesInWindowY, 5, true);
+    graphics.fillOval(screenOffsetX - 5, screenOffsetY - 5, 10, 10);
+    Coordinate test = new Coordinate(1,0);
+    test = test.getIsometricFromTile(map);
+    graphics.fillOval(x - test.x - 5, y - test.y - 5, 10, 10);
   }
 
   @Override
   public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int delta) throws SlickException {
-    Input input = gameContainer.getInput();
-    moveCamera(input, delta);
+    controls(gameContainer.getInput());
+    camera.update(delta);
   }
 
-  private Coordinate toIso(Coordinate tile) {
-    Coordinate cartesian = new Coordinate();
-    cartesian.x = -tile.x * map.getTileWidth() / 2;
-    cartesian.y = -tile.y * map.getTileHeight();
-    Coordinate isometric = new Coordinate();
-    isometric.x = cartesian.x - cartesian.y + dpg.screenWidth / 2 - map.getTileWidth() / 2;
-    isometric.y = (cartesian.x + cartesian.y) / 2 + dpg.screenHeight / 2 - map.getTileHeight() / 2;
-    return isometric;
-  }
-
-  private void moveCamera(Input input, int delta) {
-    if (movingTime < cycleTime) { movingTime += delta; }
-    else {
-      movingTime = 0;
-      if (cameraTile.y > 0 && (input.isKeyDown(Input.KEY_W) || input.isKeyDown(Input.KEY_UP))) {
-        cameraTile.y--;
-        System.out.println(cameraTile.x + " " + cameraTile.y);
-      }
-      if (cameraTile.x > 0 && (input.isKeyDown(Input.KEY_A) || input.isKeyDown(Input.KEY_LEFT))) {
-        cameraTile.x--;
-        System.out.println(cameraTile.x + " " + cameraTile.y);
-      }
-      if (cameraTile.y < map.getHeight() - 1 && (input.isKeyDown(Input.KEY_S) || input.isKeyDown(Input.KEY_DOWN))) {
-        cameraTile.y++;
-        System.out.println(cameraTile.x + " " + cameraTile.y);
-      }
-      if (cameraTile.x < map.getWidth() - 1 && (input.isKeyDown(Input.KEY_D) || input.isKeyDown(Input.KEY_RIGHT))) {
-        cameraTile.x++;
-        System.out.println(cameraTile.x + " " + cameraTile.y);
-      }
-    }
+  public void controls(Input input){
+    camera.moveLeft = input.isKeyDown(Input.KEY_A) || input.isKeyDown(Input.KEY_LEFT);
+    camera.moveRight = input.isKeyDown(Input.KEY_D) || input.isKeyDown(Input.KEY_RIGHT);
+    camera.moveUp = input.isKeyDown(Input.KEY_W) || input.isKeyDown(Input.KEY_UP);
+    camera.moveDown = input.isKeyDown(Input.KEY_S) || input.isKeyDown(Input.KEY_DOWN);
+    camera.zoomOut = input.isKeyDown(Input.KEY_O);
+    camera.zoomIn = input.isKeyDown(Input.KEY_I);
   }
 }
