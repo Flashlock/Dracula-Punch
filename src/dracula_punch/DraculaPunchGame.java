82358758f43d3c731674f229b7967640efa4fadc
Austin Betts <65870500+Flashlock@users.noreply.github.com>
2021-11-11 21:41:51 -0800

    Import Amanda Run Sprite Sheets.

--- src/dracula_punch/DraculaPunchGame.java
+++ src/dracula_punch/DraculaPunchGame.java
@@ -1,6 +1,7 @@
 package dracula_punch;
 
 import dracula_punch.States.TestState;
+import jig.ResourceManager;
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.SlickException;
@@ -13,6 +14,11 @@ public class DraculaPunchGame extends StateBasedGame {
   //endregion
 
   public static final String MAP = "dracula_punch/Resources/Tiled/test.tmx";
+
+  public static final String AMANDA_RUN_0_DEG = "dracula_punch/Resources/Sprite_Sheets/Amanda_Run/Amanda_Run_0_Deg.png";
+  public static final String AMANDA_RUN_90_DEG = "dracula_punch/Resources/Sprite_Sheets/Amanda_Run/Amanda_Run_90_Deg.png";
+  public static final String AMANDA_RUN_180_DEG = "dracula_punch/Resources/Sprite_Sheets/Amanda_Run/Amanda_Run_180_Deg.png";
+  public static final String AMANDA_RUN_270_DEG = "dracula_punch/Resources/Sprite_Sheets/Amanda_Run/Amanda_Run_270_Deg.png";
 
   public static int screenWidth, screenHeight;
 
@@ -27,6 +33,11 @@ public class DraculaPunchGame extends StateBasedGame {
   @Override
   public void initStatesList(GameContainer gameContainer) throws SlickException {
     addState(new TestState());
+
+    ResourceManager.loadImage(AMANDA_RUN_0_DEG);
+    ResourceManager.loadImage(AMANDA_RUN_90_DEG);
+    ResourceManager.loadImage(AMANDA_RUN_180_DEG);
+    ResourceManager.loadImage(AMANDA_RUN_270_DEG);
   }
 
   public static void main(String[] args) {
