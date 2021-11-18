370e221b240d96939d42299c20005f21cc64708c
Amanda <hi.amanda.s@gmail.com>
2021-11-17 17:52:35 -0800

    update to correct tileset name

--- src/dracula_punch/Resources/Tiled/spritesheet-2.tsx
+++ src/dracula_punch/Resources/Tiled/spritesheet-2.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.2" name="spritesheet-2" tilewidth="256" tileheight="288" tilecount="63" columns="9">
- <image source="spritesheet-2.png" trans="ff00ff" width="2304" height="2016"/>
+ <image source="spritesheet-2.png" width="2304" height="2016"/>
 </tileset>
