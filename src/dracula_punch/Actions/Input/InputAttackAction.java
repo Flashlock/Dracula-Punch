2e78f41e220bef8b6f136bece17784399017bd42
Austin Betts <65870500+Flashlock@users.noreply.github.com>
2021-11-29 21:22:08 -0800

    Move sheet determination into action. I did this to keep the CharacterController attackAnim function as open as possible.

--- src/dracula_punch/Actions/Input/InputAttackAction.java
+++ src/dracula_punch/Actions/Input/InputAttackAction.java
@@ -1,19 +1,53 @@
 package dracula_punch.Actions.Input;
 
 import dracula_punch.Actions.Action;
+import dracula_punch.Characters.AmandaController;
+import dracula_punch.Characters.AustinController;
 import dracula_punch.Characters.CharacterController;
-import jig.Vector;
+import dracula_punch.Characters.RittaController;
+import dracula_punch.DraculaPunchGame;
 
 public class InputAttackAction extends Action {
-    private CharacterController controller;
+    private final CharacterController controller;
+    private final DraculaPunchGame.charIdEnum charID;
 
     public InputAttackAction(CharacterController controller){
         this.controller = controller;
+        if(controller instanceof AustinController)
+            charID = DraculaPunchGame.charIdEnum.AUSTIN;
+        else if(controller instanceof AmandaController)
+            charID = DraculaPunchGame.charIdEnum.AMANDA;
+        else if(controller instanceof RittaController)
+            charID = DraculaPunchGame.charIdEnum.RITTA;
+        else{
+            charID = DraculaPunchGame.charIdEnum.UNCHOSEN;
+            System.out.println("Unknown Character Controller: ");
+        }
     }
 
     @Override
     public void Execute() {
-        if(controller.getInputLock()) return;
-        controller.animateAttack();
+        if(controller.getAnimLock()) return;
+
+        int width, height;
+        String sheet;
+        switch (charID){
+            case AUSTIN:
+                sheet = controller.getMeleeSheet();
+                width = controller.getMeleeWidth();
+                height = controller.getMeleeHeight();
+                break;
+            case AMANDA:
+            case RITTA:
+                sheet = controller.getRangedSheet();
+                width = controller.getRangedWidth();
+                height = controller.getRangedHeight();
+                break;
+            default:
+                System.out.println("Unknown Character Controller");
+                return;
+        }
+
+        controller.animateAttack(sheet, width, height);
     }
 }
